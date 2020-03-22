a04f21d7cafdb85a1ad77591be81670b37246522
divyansh0608 <divyansh8june@yahoo.com>
2020-03-22 19:27:58 +0530

    added build

---
+++ BACKEND/build/precache-manifest.2a3a4bcefc258497359f9f44783ca188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daccc03cda35071fd2c5ad2cf6a2ee0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7afef3eec6afd00cf367",
+    "url": "/static/css/main.b74cdeab.chunk.css"
+  },
+  {
+    "revision": "bbe3d797e5c78e715814",
+    "url": "/static/js/2.4454f6bf.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.4454f6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afef3eec6afd00cf367",
+    "url": "/static/js/main.4741707d.chunk.js"
+  },
+  {
+    "revision": "69dbd878029601957655",
+    "url": "/static/js/runtime-main.f8344a62.js"
+  }
+]);
